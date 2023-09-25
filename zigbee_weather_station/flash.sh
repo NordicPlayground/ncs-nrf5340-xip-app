74db9d4e6cdde358645cd2619fb18e19344ae7e8
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-09-25 14:04:54 +0100

    flash: Move Qspi.ini location
    Moves the location of the Qspi.ini file - provided as a sample
    for use when programming the external flash of the thingy53
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- zigbee_weather_station/flash.sh
+++
@@ -1,5 +0,0 @@
-nrfjprog -f NRF53 --coprocessor CP_NETWORK --sectorerase --program ./multiprotocol_rpmsg/zephyr/merged_CPUNET.hex
-nrfjprog -f NRF53 --sectorerase --program ./mcuboot/zephyr/zephyr.hex
-nrfjprog -f NRF53 --sectorerase --program ./intflash_signed.hex
-nrfjprog -f NRF53 --qspisectorerase --program ./qspi.hex --qspiini ../Qspi.ini --verify
-nrfjprog -f NRF53 --reset
--- zigbee_weather_station/rec.sh
+++
@@ -1,7 +0,0 @@
-nrfjprog -f NRF53 --recover
-nrfjprog -f NRF53 --coprocessor CP_NETWORK --recover
-nrfjprog -f NRF53 --coprocessor CP_NETWORK --sectorerase --program ./802154_rpmsg/zephyr/merged_CPUNET.hex
-nrfjprog -f NRF53 --sectorerase --program ./mcuboot/zephyr/zephyr.hex
-nrfjprog -f NRF53 --sectorerase --program ./zephyr/internal_flash_signed.hex
-nrfjprog -f NRF53 --qspisectorerase --program ./zephyr/qspi_flash_signed.hex --qspiini ../Qspi.ini --verify
-nrfjprog -f NRF53 --reset
